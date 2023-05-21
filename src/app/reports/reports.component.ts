4c367a923c172854cbdc19950d4a1fd1faa9119a
joel-tonuri <50507756+joel-tonuri@users.noreply.github.com>
2023-05-21 10:23:38 +1000

    2 files modified

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -7,15 +7,28 @@ import { ExpensesService } from '../services/expenses.service';
   styleUrls: ['./reports.component.css']
 })
 export class ReportsComponent  implements OnInit{
- 
+  selectedReport: string;
+  selectedTenant: string;
   totalExpenseByTenant: { [key: string]: number };
 
   constructor(private expensesService: ExpensesService) { }
 
   ngOnInit() {
-    this.calculateTotalExpenseByTenant();
   }
 
+  reports = [
+    { label: 'Total Amount per Tenant', value: 'totalExpense' },
+    { label: 'Expenses for Tenant', value: 'expensesByTenant' }
+  ];
+  tenantList: string[] = ['Fred', 'Mary', 'Phillip', 'Ryan', 'Peter'];
+  generateReport() {
+    if (this.selectedReport === 'totalExpense') {
+      // Generate total expense report
+      this.calculateTotalExpenseByTenant();
+    }
+    // Add other report generation logic for different report types
+  }
+  
   calculateTotalExpenseByTenant() {
     const expenses = this.expensesService.getExpense();
     this.totalExpenseByTenant = {};
